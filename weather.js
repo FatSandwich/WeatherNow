491fa540cf7bae01ec2e2f62e76747fe2ba1ff30
FatSandwich <63752541+FatSandwich@users.noreply.github.com>
2020-04-15 17:18:51 -0400

    Create weather.js

---
+++ weather.js
@@ -0,0 +1,37 @@
+const api = {
+  key: "c204db8646efd8ade5a73f313178d133",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+  if (evt.keyCode == 13) {
+    getResults(searchbox.value);
+  }
+}
+
+function getResults(query) {
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+    return weather.json();
+  }).then(displayResults);
+}
+
+function displayResults(weather) {
+  let city = document.querySelector('.location .city');
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let temp = document.querySelector('.current .temp');
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+
+  let weather_el = document.querySelector('.current .weather');
+  weather_el.innerText = weather.weather[0].main;
+
+  let hilow = document.querySelector('.hi-low');
+  hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+}
+
+
+//http://api.openweathermap.org/data/2.5/weather?q=Toronto&units=metric&APPID=c204db8646efd8ade5a73f313178d133
